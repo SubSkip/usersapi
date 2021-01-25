f46acb1e3ffaa86da321303b46c412e71fe0855f
SubSkip <craig.e.zieg@gmail.com>
2021-01-25 11:17:03 -0600

    added Swagger API stuff

--- src/main/java/com/tts/usersapi/User.java
+++ src/main/java/com/tts/usersapi/User.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,14 +21,18 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(notes = "The ID of the user")
     private Long id; // Id
 
+    @ApiModelProperty(notes = "The user first name")
     @Length(max = 20)
     private String fname;// First name
 
+    @ApiModelProperty(notes = "The user last name")
     @Length(min = 2)
     private String lname;// Last name
 
+    @ApiModelProperty(notes = "The user state of residence")
     @Length(min = 4, max = 20)
     private String state;// State (of residence)
 }
